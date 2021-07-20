b47c5b8aac25070724ff71d36885d9e15ad7b440
gregjhansell97 <gregjhansell@gmail.com>
2021-07-20 07:39:21 -0600

    wip

--- web/src/hooks/nearest-mc-dicks.js
+++ web/src/hooks/nearest-mc-dicks.js
@@ -1,5 +1,22 @@
 
-function nearestMcDicks(loc) {
-    // given location get new location of nearest mcdicts
-    return [1, 1];
+class BallRegion {
+    // IDEA: subregions -> returns nearest subregions ordered by location
+}
+
+class BallTree {
+    constructor(locs) {
+        this.locs = locs
+    }
+
+    function nearest(lat, lon) {
+        return this.locs[0];
+    }
+}
+
+
+
+const mcDicksBallTree = new BallTree([[1, 1, "Mc-Donalds?"]]);
+
+function nearestMcDicks([lat, lon]) {
+    return mcDicksBallTree.nearest(lat, lon);
 }
